0691b0c0b7d502b9558422da021eca7d0775235c
elisun82 <elisun82@gmail.com>
2026-04-21 19:58:42 +0300

    app.py

--- app.py
+++ app.py
@@ -578,10 +578,10 @@ if uploaded_file:
     c1, c2, c3, c4, c5 = st.columns(5)
 
     show_metric_block(c1, "ACCOMMODATION", "RevPAR", "revpar", data["revpar"])
-    show_metric_block(c2, "TOTAL F&B", "Total revenue", "FB_TotalRevenue", data["FB_TotalRevenue"])
-    show_metric_block(c3, "SERVICE", "Rev. / wtrs. Hour", "ServiceHour", data["ServiceHour"])
-    show_metric_block(c4, "KITCHEN", "Rev. / ktch. Hour", "KitchenHour", data["KitchenHour"])
-    show_metric_block(c5, "HOTEL TOTAL", "Total revenue", "HotelTotalRevenue", data["HotelTotalRevenue"])
+    show_metric_block(c2, "TOTAL F&B", "Total revenue", "fb_total_revenue", data["fb_total_revenue"])
+    show_metric_block(c3, "SERVICE", "Rev. / wtrs. Hour", "service_hour", data["service_hour"])
+    show_metric_block(c4, "KITCHEN", "Rev. / ktch. Hour", "kitchen_hour", data["kitchen_hour"])
+    show_metric_block(c5, "HOTEL TOTAL", "Total revenue", "hotel_total_revenue", data["hotel_total_revenue"])
 
     # вывод
     render_summary_block(build_summary(data))
